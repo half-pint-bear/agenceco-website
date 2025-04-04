1a1ec20c7ce88fea9b6a84a46723d65fe3b6a8d9
CrankyBear <jeremie.bozant@ipilyon.net>
2025-04-04 20:00:00 +0200

    painfully make diaporama working

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ const navMenu = document.querySelector("nav");
 burgerMenu.addEventListener('click', () => {
     navMenu.style.display === "none" ? navMenu.style.display = "block" : navMenu.style.display = "none";
 })
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    if(n > slides.length)
+        slideIndex = 1;
+
+    if(n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for(let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex -1].style.display = "block";
+}
